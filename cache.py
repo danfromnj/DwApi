bf750bc7aea2d96201362adcfda2b967ce785943
danfromnj <hudan.nj@gmail.com>
2016-10-31 00:07:52 +0800

    making user in http header part of redis namespace, also support pretty format in which query results are organized in a json element array

--- cache.py
+++ cache.py
@@ -6,14 +6,13 @@ class CacheManager(object):
     def __init__(self, host, port, db):
         self.r = redis.StrictRedis(host=host, port=port, db=db)
 
-    def gen_cachekey(self, namespace, raw_str):
-        iso8859_1 = lambda key: key.encode('ISO8859-1') \
-            if type(key) == unicode else key
+    def __gen_cachekey(self, namespace, raw_str):
+        iso8859_1 = lambda key: key.encode('ISO8859-1') if type(key) == unicode else key
         md5 = hashlib.md5(iso8859_1(raw_str))
         return namespace + ':' + md5.hexdigest()
 
-    def put(self, query_str, result, expire=300):
-        ck = self.gen_cachekey(DEFAULT_NAMESPACE, query_str)
+    def put(self, query_str, result, namespace,expire=300):
+        ck = self.__gen_cachekey(namespace, query_str)
         ret = None
         try:
             ret = self.r.setex(ck, expire, result)
@@ -21,8 +20,8 @@ class CacheManager(object):
             print str(e)
         return ret
 
-    def get(self, query_str):
-        ck = self.gen_cachekey(DEFAULT_NAMESPACE, query_str)
+    def get(self, query_str, namespace):
+        ck = self.__gen_cachekey(namespace, query_str)
         ret = None
         try:
             ret = self.r.get(ck)
